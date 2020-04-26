482503037472f49a78b65557a715e68dbf20c1f9
Jeff <salisbury.jeffery@gmail.com>
2020-04-26 11:55:02 -0400

    Adding file to read decklist.txt and adding the num field to the new list of cards with raitings in the decklist.

--- judge.js
+++ judge.js
@@ -1,20 +1,39 @@
 const app = require("express")();
 const fs = require("fs");
 const iko = require("./Output.json");
-const fetch = require("node-fetch");
+
 async function start() {
   //   console.log(iko);
-  await fetch("./decklist.txt")
-    .then((response) => response.text())
-    .then((text) => console.log(text))
-    .catch((err) => console.error(err));
+  let decklist = [];
+  fs.readFile("./decklist.txt", "utf8", function (err, data) {
+    if (err) throw err;
+    let cleanDeckList = [];
+
+    data.split(",").forEach((value) => {
+      cleanDeckList.push(value.trim());
+    });
+
+    cleanDeckList.forEach((i) => {
+      decklist.push({ num: i.slice(0, 1), name: i.slice(2) });
+    });
+
+    value_list = iko.filter((el) => {
+      return decklist.some((f) => {
+        if (f.name === el.name) {
+          el.num = f.num;
+          return true;
+        }
+      });
+    });
+
+    console.log({ value_list: value_list });
+  });
 }
 
 start();
 
-app.get("/", async (req, res) => res.send({ cards }));
+app.get("/", async (req, res) => res.send({ decklist }));
 
-// app.listen(3000);
 app.listen(3000, () =>
   console.log(`Example app listening at http://localhost:${3000}`)
 );
