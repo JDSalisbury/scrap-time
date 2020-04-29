4ad0a9fc822933ee83a1133ed68b821a8a69bad6
Jeff <salisbury.jeffery@gmail.com>
2020-04-28 23:46:43 -0400

    Adding the total for just the mainboard now, breaking out sideboard now. Then to refactor and breack out functions.

--- judge.js
+++ judge.js
@@ -6,29 +6,43 @@ var obj = {};
 async function start() {
   //   console.log(iko);
   const decklist = [];
+  const sideboard = [];
   let deck_min = 0;
   let deck_max = 0;
-  fs.readFile("./mtgadecklist.txt", "utf8", function (err, data) {
-    if (err) throw err;
 
+  fs.readFile("./mtgdeck2.txt", "utf8", function (err, data) {
+    if (err) throw err;
+    let main_board = true;
     data.split("\n").forEach((v) => {
       vt = v.trim();
-      // if (vt === "Sideboard") {
-      //   break;
-      // }
-      decklist.push({ num: vt.slice(0, 1), name: vt.slice(2) });
-    });
-
-    const value_list = iko.filter((el) => {
-      return decklist.some((f) => {
-        if (f.name.includes(el.name)) {
-          el.num = parseInt(f.num);
-          return true;
+      if (vt === "Sideboard") {
+        main_board = false;
+      }
+      if (main_board) {
+        if (parseInt(vt.slice(0, 1)) > 0) {
+          decklist.push({ num: parseInt(vt.slice(0, 1)), name: vt.slice(2) });
         }
-      });
+      } else {
+        sideboard.push({ num: parseInt(vt.slice(0, 1)), name: vt.slice(2) });
+      }
     });
 
-    value_list.forEach((e) => {
+    function deckListWithSetValue(set, cardlist) {
+      return set.filter((el) => {
+        return cardlist.some((f) => {
+          if (f.name.includes(el.name)) {
+            el.num = f.num;
+            return true;
+          }
+        });
+      });
+    }
+
+    const main = deckListWithSetValue(iko, decklist);
+
+    // const side = deckListWithSetValue(iko, sideboard);
+
+    main.forEach((e) => {
       ev = e.value;
       val = parseFloat(ev) * e.num;
       if (ev.length > 3) {
@@ -40,7 +54,7 @@ async function start() {
     obj = {
       draft_min: deck_min,
       draft_max: deck_min + deck_max,
-      drafted_cards: value_list,
+      drafted_cards: main,
     };
     console.log(obj);
   });
